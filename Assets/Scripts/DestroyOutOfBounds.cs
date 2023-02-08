21cd687277b2b2f7626be5e50a6d3351d74b384a
Amaya Reed <amreed1@s.sfusd.edu>
2023-02-08 11:02:13 -0800

    started spawnmanager

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class D : MonoBehaviour
+public class DestroyOutOfBounds : MonoBehaviour
 {
 	private float topBound = 30;
+    private float lowerBound = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +15,13 @@ public class D : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-	if (transform.position.z > topBound) {
-	 Destroy(gameObject); }}
+        if (transform.position.z > topBound) 
+        {
+            Destroy(gameObject); 
+        } else if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+        }
         
     }
 }
