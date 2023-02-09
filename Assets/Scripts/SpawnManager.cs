6ba55a3f78947bc36fe839e80b9bb109f45234e2
Amaya Reed <amreed1@s.sfusd.edu>
2023-02-08 22:59:39 -0800

    still spawning

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
     
     // Start is called before the first frame update
     void Start()
@@ -17,9 +19,10 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S)) 
         {
-            int animalIndex = 
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range( -spawnRangeX, spawnRangeX), 0, spawnPosZ);
             
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0,0,20), animalPrefabs[animalIndex].transform.rotation);
+            Instantiate(animalPrefabs[animalIndex], animalPrefabs[animalIndex].transform.rotation);
         }
     }
 }
