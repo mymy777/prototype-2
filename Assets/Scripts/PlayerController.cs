b6cce18884ca783b7fa31e7b34e94423872aa7d1
Amaya Reed <amreed1@s.sfusd.edu>
2023-02-10 15:44:51 -0800

    got stuck at 2.4 bc my cookies went away

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -28,13 +28,13 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
 	
-	if (Input.GetKeyDown(KeyCode.Space))
-	{
-		//Launch a projectile from the player
+	    if (Input.GetKeyDown(KeyCode.Space)) 
+        {
 	     Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
 	   }
     }
