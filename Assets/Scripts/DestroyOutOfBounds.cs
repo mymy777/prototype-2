ce2b2dda15349c3d8d99e987c28f67e7277ae5b5
Amaya Reed <amreed1@s.sfusd.edu>
2023-02-15 11:02:03 -0800

    objects werent getting destroyed when colliding

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
-	private float topBound = 30;
+	private float topBound = 60;
     private float lowerBound = -5;
     // Start is called before the first frame update
     void Start()
